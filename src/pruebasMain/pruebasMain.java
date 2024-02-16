43b2cfac0dd97cb1489620407b8ba0a472ec20b1
LATITUDE E5440 <LATITUDE E5440@192.168.95.1>
2024-02-16 12:30:02 -0600

    se add class Producto

---
+++ src/pruebasMain/pruebasMain.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebasMain;
+
+/**
+ *
+ * @author LATITUDE E5440
+ */
+public class pruebasMain {
+    public static void main(String[] args) {
+        
+        Producto a = new Producto("leche", 12.5);
+        
+        
+        
+    }
+}
