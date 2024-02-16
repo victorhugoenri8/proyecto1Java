74863704c74a695591f774f92d1422a76f4c5879
LATITUDE E5440 <LATITUDE E5440@192.168.95.1>
2024-02-16 11:46:42 -0600

    primera prueba repo neatbens udemy1

---
+++ src/orden/Orden.java
@@ -0,0 +1,27 @@
+
+package orden;
+
+
+public class Orden {
+    
+    private int idOrden;
+    private static int contadorOrdenes;
+    String [] productos = {"Producto"};
+    static int MAXPRODUCTOS = 10;
+
+    public void Orden (idorder,contadororden) {}
+    
+    public void calcularTotal (){
+        
+    }
+    
+    public void agregarProducto(String productos) {
+        
+       
+    }
+    
+    public void mostrarOrden(){
+        
+    }
+    
+}
