3961fe92a0722ee32bc6e5d68a7caeebf35b2f9b
victorhugoenri8 <victorhugoenriquezco@gmail.com>
2024-02-17 23:39:27 -0600

    pruebas de class producto por victor

--- src/pruebasMain/pruebasMain.java
+++ src/pruebasMain/pruebasMain.java
@@ -4,6 +4,8 @@
  */
 package pruebasMain;
 
+import orden.Producto;
+
 /**
  *
  * @author LATITUDE E5440
@@ -12,8 +14,12 @@ public class pruebasMain {
     public static void main(String[] args) {
         
         Producto a = new Producto("leche", 12.5);
-        
-        
-        
+	
+        Producto  b = new Producto("gato", 1.5);
+	    System.out.println(a );
+
+	    System.out.println( b);
+	    
+        		System.out.println(a.getIdProducto());
     }
 }
